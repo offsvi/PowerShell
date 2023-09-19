c8cc6b4683b93a24510ef49dc9bacf632419edf9
Vitor Oliveira <vitorsk807@gmail.com>
2023-09-18 22:28:41 -0300

    Estudo da variavel FOREACH

---
+++ Exemplo-Foreach.ps1
@@ -0,0 +1,9 @@
+﻿# EXEMPLO FOREACH
+
+Clear-Host
+#foreach ($numeros in 1,2,3,4,5,6,7,8,9) {echo $numeros}
+
+foreach ($arquivos in Get-ChildItem) {
+if ($arquivos.IsReadOnly){
+Write-Host $arquivos.FullName}
+}
